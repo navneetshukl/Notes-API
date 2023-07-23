3bbd572d5865f9532a08d29ce868b5af0f4d9c32
Navneet shukla <navneetshukla824@gmail.com>
2023-07-23 23:32:04 +0530

    Made some changes in code

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -15,25 +15,20 @@ import (
 )
 
 func Signup(c *gin.Context) {
-
-	// Get the data from the body
-
 	var body struct {
-		Name     string
-		Email    string
-		Password string
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
 	}
-	if c.Bind(&body) != nil {
+
+	if err := c.Bind(&body); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Failed to read request body",
 		})
 		return
 	}
 
-	// Hash the Password
-
 	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
-
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Error hashing password",
@@ -41,8 +36,13 @@ func Signup(c *gin.Context) {
 		return
 	}
 
-	// Create the User
-	DB, _ := database.ConnectToDatabase()
+	DB, err := database.ConnectToDatabase()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to connect to database",
+		})
+		return
+	}
 
 	user := models.User{
 		Name:     body.Name,
@@ -50,41 +50,42 @@ func Signup(c *gin.Context) {
 		Password: string(hash),
 	}
 
-	result:=DB.Create(&user)
-
-	if result.Error!=nil{
-		c.JSON(http.StatusBadRequest,gin.H{
-			"error":"Unable To create user in Database ",
+	result := DB.Create(&user)
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Unable to create user in the database",
 		})
 		return
 	}
 
-	//Respond
-
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Signup Successfully",
+		"message": "Signup Successful",
 	})
-
 }
 
 func Login(c *gin.Context) {
-
-	//Get the email and password off requested body
 	var body struct {
-		Email    string
-		Password string
+		Email    string `json:"email"`
+		Password string `json:"password"`
 	}
-	if c.Bind(&body) != nil {
+
+	if err := c.Bind(&body); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Failed to read request body",
 		})
 		return
 	}
 
-	// Look up requested user
-	DB,_:=database.ConnectToDatabase()
+	DB, err := database.ConnectToDatabase()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to connect to database",
+		})
+		return
+	}
+
 	var user models.User
-	DB.First(&user,"email=?",body.Email)
+	DB.First(&user, "email=?", body.Email)
 
 	if user.ID == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -93,22 +94,13 @@ func Login(c *gin.Context) {
 		return
 	}
 
-	//Compare password
-
-	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
-
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Invalid Email or Password",
 		})
 		return
 	}
-
-	c.JSON(200, gin.H{
-		"messaage from login": user,
-	})
-
-	//Generate a JWT token
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"sub": user.ID,
@@ -119,30 +111,28 @@ func Login(c *gin.Context) {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	secret := os.Getenv("SECRET")
 
+	secret := os.Getenv("SECRET")
 	tokenString, err := token.SignedString([]byte(secret))
 
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Failed to create token",
 		})
+		return
 	}
 
-	//send it back
-
 	c.SetSameSite(http.SameSiteLaxMode)
-	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", false, true)
+	c.SetCookie("Authorization", tokenString, int(time.Hour*24*30), "/", "", false, true)
 
-	c.JSON(http.StatusOK, gin.H{})
-
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Login Successful",
+	})
 }
 
 func Validate(c *gin.Context) {
-
 	user, _ := c.Get("user")
 	c.JSON(http.StatusOK, gin.H{
 		"user": user,
 	})
-
 }
